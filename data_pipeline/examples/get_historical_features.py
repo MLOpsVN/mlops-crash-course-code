3df22c1ce594b22ef1587ac6bea1b60100e4b6af
quan-dang <s1232003@u-aizu.ac.jp>
2022-09-10 11:10:11 +0700

    add example to get online feature

--- data_pipeline/examples/get_historical_features.py
+++ data_pipeline/examples/get_historical_features.py
@@ -1,18 +1,17 @@
 from feast import FeatureStore
-
-store = FeatureStore(repo_path="../feature_repo")
-
 import pandas as pd
 from datetime import datetime
+
+store = FeatureStore(repo_path="../feature_repo")
 
 entity_df = pd.DataFrame.from_dict(
     {
         "driver_id": [1001, 1002, 1003, 1004, 1001],
         "event_timestamp": [
-            datetime(2021, 4, 12, 10, 59, 42),
-            datetime(2021, 4, 12, 8, 12, 10),
-            datetime(2021, 4, 12, 16, 40, 26),
-            datetime(2021, 4, 12, 15, 1, 12),
+            datetime(2022, 4, 12, 10, 59, 42),
+            datetime(2022, 4, 12, 8, 12, 10),
+            datetime(2022, 4, 12, 16, 40, 26),
+            datetime(2022, 4, 12, 15, 1, 12),
             datetime.now()
         ]
     }
---
+++ data_pipeline/examples/get_online_features.py
@@ -0,0 +1,21 @@
+from feast import FeatureStore
+
+store = FeatureStore(repo_path="../feature_repo")
+
+features = store.get_online_features(
+    features=[
+        "driver_stats:acc_rate",
+        "driver_stats:conv_rate"
+    ],
+    entity_rows=[
+        {
+            "driver_id": 1001,
+        }
+    ],
+).to_dict(include_event_timestamps=True)
+
+def print_online_features(features):
+    for key, value in sorted(features.items()):
+        print(key, " : ", value)
+
+print_online_features(features)
