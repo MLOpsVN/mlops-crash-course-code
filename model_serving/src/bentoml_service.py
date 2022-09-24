38d8e282b8ca31a8fe2b2d91ca41d65ff3bd9357
Tung Dao <tungdao17@gmail.com>
2022-09-24 11:49:53 +0900

    Refactor bentoml service

--- model_serving/src/bentoml_service.py
+++ model_serving/src/bentoml_service.py
@@ -5,6 +5,7 @@ import feast
 import mlflow
 import numpy as np
 from bentoml.io import JSON, NumpyNdarray
+from mlflow.models.signature import ModelSignature
 from pydantic import BaseModel
 
 from utils import *
@@ -31,6 +32,12 @@ def save_model() -> bentoml.Model:
     mlflow_model = mlflow.pyfunc.load_model(model_uri=model_uri)
     Log().log.info(mlflow_model.__dict__)
     model = mlflow_model._model_impl
+    model_signature: ModelSignature = mlflow_model.metadata.signature
+
+    # construct feature list
+    feature_list = []
+    for name in model_signature.inputs.input_names():
+        feature_list.append(name)
 
     # save model using bentoml
     bentoml_model = bentoml.sklearn.save_model(
@@ -50,23 +57,26 @@ def save_model() -> bentoml.Model:
             "mlflow_model_name": model_name,
             "mlflow_model_version": model_version,
         },
+        custom_objects={
+            "feature_list": feature_list,
+        },
     )
     Log().log.info(bentoml_model)
     return bentoml_model
 
 
 bentoml_model = save_model()
+feature_list = bentoml_model.custom_objects["feature_list"]
 bentoml_runner = bentoml.sklearn.get(bentoml_model.tag).to_runner()
 svc = bentoml.Service(bentoml_model.tag.name, runners=[bentoml_runner])
-
 fs = feast.FeatureStore(repo_path=AppPath.FEATURE_REPO)
 
 
-class ClassifyRequest(BaseModel):
+class InferenceRequest(BaseModel):
     driver_ids: List[int]
 
 
-class ClassifyResponse(BaseModel):
+class InferenceResponse(BaseModel):
     prediction: Optional[float]
     error: Optional[str]
 
@@ -83,31 +93,28 @@ def predict(request: np.ndarray) -> np.ndarray:
 
 
 @svc.api(
-    input=JSON(pydantic_model=ClassifyRequest),
-    output=JSON(pydantic_model=ClassifyResponse),
+    input=JSON(pydantic_model=InferenceRequest),
+    output=JSON(pydantic_model=InferenceResponse),
 )
-def classify(request: ClassifyRequest, ctx: bentoml.Context) -> Dict[str, Any]:
+def inference(request: InferenceRequest, ctx: bentoml.Context) -> Dict[str, Any]:
     """
     Example request: {"driver_ids":[1001,1002,1003,1004,1005]}
     """
-    Log().log.info(f"start classify")
-    response = ClassifyResponse()
+    Log().log.info(f"start inference")
+    response = InferenceResponse()
     try:
         Log().log.info(f"request: {request}")
         driver_ids = request.driver_ids
 
-        df = fs.get_online_features(
+        online_features = fs.get_online_features(
             entity_rows=[{"driver_id": driver_id} for driver_id in driver_ids],
-            features=[
-                "driver_stats:conv_rate",
-                "driver_stats:acc_rate",
-                "driver_stats:avg_daily_trips",
-            ],
+            features=[f"driver_stats:{name}" for name in feature_list],
         )
-        df = pd.DataFrame.from_dict(df.to_dict())
+        df = pd.DataFrame.from_dict(online_features.to_dict())
         Log().log.info(f"online features: {df}")
 
         input_features = df.drop(["driver_id"], axis=1)
+        input_features = input_features[feature_list]
         Log().log.info(f"input_features: {input_features}")
 
         result = predict(input_features[sorted(input_features)])
